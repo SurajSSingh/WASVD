f385954ae194f0ad8a0b79486bbe991d8838357e
SurajSSingh <22825536+SurajSSingh@users.noreply.github.com>
2023-11-13 12:06:16 -0800

    feat: :sparkles: Finish Tauri tutorial
    Complete guide for setting up Tauri with SvelteKit

---
+++ src/lib/component/Greet.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { invoke } from '@tauri-apps/api/tauri';
+
+	let name = '';
+	let greetMsg = '';
+
+	async function greet() {
+		greetMsg = await invoke('greet', { name });
+	}
+</script>
+
+<div>
+	<input id="greet-input" placeholder="Enter a name..." bind:value={name} class=" bg-primary-800" />
+	<button on:click={greet} class="bg-primary-400">Greet</button>
+	<p>{greetMsg}</p>
+</div>
