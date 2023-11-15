ba8d6505ecc6893e269e5ad08397edc966333a52
SurajSSingh <22825536+SurajSSingh@users.noreply.github.com>
2023-11-14 23:55:03 -0800

    feat: :construction: WIP: Modularize Rust and Improve Svelte Viz

--- src/lib/component/Editor.svelte
+++ src/lib/component/Editor.svelte
@@ -2,7 +2,8 @@
 	import CodeMirror from 'svelte-codemirror-editor';
 	import { wast } from '@codemirror/lang-wast';
 	import { invoke } from '@tauri-apps/api/tauri';
-	import * as command from '../../bindings';
+	import * as command from '$lib/bindings';
+	import { combine_bytes, instruction_in_plain_english } from '$lib';
 
 	let text = '(module)';
 	let result: command.InterpreterStructure | null = null;
@@ -42,32 +43,57 @@
 		<p class=" bg-error-700">{JSON.stringify(error, null, 4)}</p>
 	{:else if result}
 		<section class=" text-black">
-			{#each result.func as f}
-				<h2>{f.name}</h2>
-				<h3>Parameters</h3>
-				<ul>
-					{#each f.parameters as param, i}
-						<li>{param[0] ?? i}: {param[1]}</li>
-					{/each}
-				</ul>
-				<h3>Locals</h3>
-				<ul>
-					{#each f.locals as loc, i}
-						<li>{loc[0] ?? i}: {loc[1]}</li>
-					{/each}
-				</ul>
-				<h3>Instructions</h3>
-				<ul>
-					{#each f.body as instruction, i}
-						<li>Step {i + 1}: {JSON.stringify(instruction)}</li>
-					{/each}
-				</ul>
-				<h3>Return</h3>
-				<ul>
-					{#each f.result as res, i}
-						<li>{i}: {res}</li>
-					{/each}
-				</ul>
+			{#each Object.entries(result.exported) as [name, [kind, index]], _ (name)}
+				{#if kind === 'Function'}
+					{@const f = result.func.at(index)}
+					{#if f}
+						<h2>Function {f.name ?? name}</h2>
+						<h3>Parameters</h3>
+						<ul>
+							{#each f.parameters as param, i}
+								<li>{param[0] ?? i}: {param[1]}</li>
+							{/each}
+						</ul>
+						<h3>Locals</h3>
+						<ul>
+							{#each f.locals as loc, i}
+								<li>{loc[0] ?? i}: {loc[1]}</li>
+							{/each}
+						</ul>
+						<h3>Instructions</h3>
+						<ul>
+							{#each f.body as instruction, i}
+								<li><strong>Step {i + 1}</strong>: {instruction_in_plain_english(instruction)}</li>
+							{/each}
+						</ul>
+						<h3>Return</h3>
+						<ul>
+							{#each f.result as res, i}
+								<li>{i}: {res}</li>
+							{/each}
+						</ul>
+					{:else}
+						<h1 class=" bg-error-500">No function at index {index} for exported name {name}</h1>
+					{/if}
+				{:else if kind === 'Global'}
+					<h1>Global {name}</h1>
+				{:else if kind === 'Memory'}
+					{@const m = result.memory.at(index)}
+					{#if m}
+						<h2>Memory {m.name} (exported as {name})</h2>
+						<h3>Properties</h3>
+						<ul>
+							<li>Is 32-bit: {m.is_32}</li>
+							<li>Min Size: {combine_bytes(m.min_lower, m.min_upper)}</li>
+							<li>Max Size: {combine_bytes(m.max_lower, m.max_upper)}</li>
+							<li>Is Shared: {m.is_shared}</li>
+						</ul>
+						<h3>Current Data</h3>
+						<p>{m.data}</p>
+					{:else}
+						<h1 class=" bg-error-500">No function at index {index} for exported name {name}</h1>
+					{/if}
+				{/if}
 			{/each}
 		</section>
 		<p class=" bg-success-700">{JSON.stringify(result, null, 4)}</p>
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,44 @@
 // place files you want to import through the `$lib` alias in this folder.
+import type * as command from "$lib/bindings"
+
+export function combine_bytes(lower: number, upper:number): bigint{
+    return (BigInt(upper) << BigInt(32)) | BigInt(lower);
+}
+
+export function instruction_in_plain_english(instruction: command.SerializedInstruction):string {
+    if (typeof instruction === "string"){
+        switch (instruction) {
+            case "Unreachable":
+                return "Unreachable"
+            case "Nop":
+                return "Do nothing"
+            case "Drop":
+                return "Drop the current top value of the stack"
+            case "Return":
+                return "Return immediately"
+            default:
+                return JSON.stringify(instruction);
+        }
+    } else {
+        if("Get" in instruction){
+            return `Get ${instruction.Get.is_local? "local" : "global"} value '${instruction.Get.loc}' and push it on to the stack.`
+        }
+        else if("Set" in instruction){
+            return `Set ${instruction.Set.is_local? "local" : "global"} value '${instruction.Set.loc}' to the current value on the stack`
+        }
+        else if("Tee" in instruction){
+
+            return `Set the local value '${instruction.Tee.loc}' to the current value on the stack and immediately push it back onto the stack.`
+        }
+        else if ("ControlFlow" in instruction){
+            const cf = instruction.ControlFlow;
+            if("End" in cf){
+                return `End of block ${cf.End.length > 0 ? cf.End : ""}`
+            }
+            else if("Else" in cf){
+                return `Else ${cf.Else.length > 0 ? `(label: ${cf.Else})` : ""}`
+            }
+        }
+    }
+    return JSON.stringify(instruction);
+}
